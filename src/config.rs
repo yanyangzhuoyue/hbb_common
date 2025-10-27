1d5e7f081a2c501646813012cc9c7240381c5f56
yanyangzhuoyue <1418134850@qq.com>
2025-10-27 23:19:27 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -101,8 +101,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["desk.easyuser.top"];
+pub const RS_PUB_KEY: &str = "Y9IbtWafb5AuUzsuPO4SP6pxTNjIaMN6ZfxFDm2CLVk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
